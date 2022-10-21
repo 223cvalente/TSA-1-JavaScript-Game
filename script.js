49b6febe3b816dadb0e7bc2f8c6282601d97929a
ChristopherV3 <84456226+223cvalente@users.noreply.github.com>
2022-10-21 14:43:29 +0000

    continue

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded' , () => {
     const ground = document.querySelector('.ground-moving')
 
     let birdLeft = 220
-       let birdBottom = 100
+    let birdBottom = 100
     let gravity = 3
     let isGameOver = false
     let gap = 430
@@ -23,9 +23,6 @@ document.addEventListener('DOMContentLoaded' , () => {
             jump()
         }
     }
-
- 
-
     function jump() {
         if (birdBottom < 500) birdBottom += 50
         bird.style.bottom = birdBottom + 'px'
