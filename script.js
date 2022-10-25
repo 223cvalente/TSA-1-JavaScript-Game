52b147e09ddad93e7ea9fc5637675487cd2c431b
ChristopherV3 <84456226+223cvalente@users.noreply.github.com>
2022-10-25 12:29:59 +0000

    continue

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded' , () => {
 
   
     function control(e) {
-        if (e.keyCode = 32) {
+        if (e.keyCode = 55) {
             jump()
         }
     }
